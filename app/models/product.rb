fc4b34643aac006821c21813e7f84aaa203078ed
Travis Eubanks <tmeubanks113@gmail.com>
2015-08-30 09:50:58 -0500

    Sets up styling for sidebar and tests data is there

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,13 @@ class Product < ActiveRecord::Base
    validates :price, numericality: {:greater_than_or_equal_to => 0.01}
    validates :title, uniqueness: true
    validates :title, length: { maximum: 10 }
+
    validates :image_url, allow_blank: true, format: {
-                  with: %r{\.(gif|jpg|png)\Z},
-                  message: 'Must be a URL for GIF, JPG or PNG image.'
-               }
+      with: %r{\.(gif|jpg|png)\Z},
+      message: 'Must be a URL for GIF, JPG or PNG image.'
+   }
+
+   def self.latest
+      Product.order(:updated_at).last
+   end
 end
