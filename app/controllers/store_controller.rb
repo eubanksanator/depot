9815117de83f521de76e40510221f0a6a8461d37
Travis Eubanks <tmeubanks113@gmail.com>
2015-08-31 08:00:36 -0500

    end of chapter 9

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
+   @count = index_view_counter
    @products = Product.all.order(:title)
   end
+
+  private
+   def index_view_counter
+      session[:counter] ||= 0
+      session[:counter] += 1
+   end
 end
